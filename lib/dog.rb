025ce3d8a253ae93e4fd42c4deb0bcf0d8a6fbc1
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 20:39:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,7 +16,7 @@ class Dog
   end
 
   def self.print_all
-    @@all.each {|pup| puts "#{@name}\n"}
+    @@all.each {|pup| puts "#{}\n"}   # expect(Dog.instance_variable_get()@all)).to match([])
   end
 
   def self.clear_all
